b009b52b266ab193c4f71158a24b901e62108f32
David Walker <dwalker27@gmail.com>
2021-09-21 18:30:49 -0400

    Reminder of passing by reference

---
+++ Meet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+void swap(int *a, int *b)
+{
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+int main(void)
+{
+  int x = 10;
+  int y = 20;
+
+  printf("Initial values: \n x = %d; y = %d \n\n", x, y);
+
+  swap(&x, &y);
+
+  printf("Values after calling swap: \n x = %d; y = %d \n", x, y);
+
+  return 0;
+}
